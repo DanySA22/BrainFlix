f434393ba271daecac381005bfb449b8697c0d53
Daniel Savignon Araujo <winache22@gmail.com>
2024-02-22 17:03:42 -0800

    Adding json file

--- brainflix-daniel/src/App.jsx
+++ brainflix-daniel/src/App.jsx
@@ -10,40 +10,73 @@ import sideData from './data/videos.json'
 function App() {
   const [dataDetails, setDataDetails] = useState(mainData)
   const [data, setData] = useState(sideData)
+  const dataMainDetails = dataDetails
   
+  const videoMain = dataMainDetails.filter((dataMainDetail) => (dataMainDetail.id == '84e96018-4022-434e-80bf-000ce4cd12b8')).map((dataMainDetail) => (
+    <video className='video-Image' controls poster= {dataMainDetail.image} key={dataMainDetail.id}>
+    </video>
+  ))
+
+  const listDataMain = dataMainDetails.filter((dataMainDetail) => (dataMainDetail.id == '84e96018-4022-434e-80bf-000ce4cd12b8')).map((dataMainDetail) => (
+    <section className='main-video' key={dataMainDetail.id}>
+    <h2 className='main-video__header'> {dataMainDetail.title} </h2>
+    <div className='main-video__info'>
+    <div className='main-video__info1'> 
+    <p className='main-video__author'> <span>By</span> {dataMainDetail.channel}</p>
+    <p className='main-video__date'> {dataMainDetail.timestamp}  </p> 
+    </div> 
+    <div className='main-video__info2'>
+     <p className='main-video__views'> {dataMainDetail.views} </p>
+     <p className='main-video__likes'> {dataMainDetail.likes} </p>
+    </div> 
+    </div> 
+    <div className='main-video__details'>
+    <p className='main-video__text'>
+    {dataMainDetail.description}
+    </p> 
+    </div> 
+ </section>
+  ))
+console.log(dataMainDetails[0].comments[1].name)
+ const commentMain = dataMainDetails.filter((dataMainDetail) => (dataMainDetail.id == '84e96018-4022-434e-80bf-000ce4cd12b8')).map((dataMainDetail) => (
+  <section className='comment' key={dataMainDetail.id}>
+  <h2 className="comment__amount"> {dataMainDetail.comments.length} Comments </h2>
+  <div className="comment__subdivision">
+      <img src={mohan}  className="comment__image" alt="Mohan Muruge image" />
+      <form action="" className="comment__form">
+          <label className="comment__form-Label"> JOIN THE CONVERSATION </label>
+          <textarea name="nameInput" className="comment__form-Input">
+           Add a new comment
+          </textarea>
+          <button className="comment__submit-Button"> <p className="comment__submit-Button--text"> COMMENT </p></button>
+      </form> 
+    </div >
+    <div className = 'comment__display'>
+    <div  className = 'comment__user'>
+      <img className = 'comment__user-Image'/>
+      <div className = 'comment__user-Info'> 
+        <p className = 'comment__user-Name'> Noah Duncan </p>
+        <p className = 'comment__user-Date'>  8/11/2023 </p>
+        <p className = 'comment__user-Text'> 
+        Your insights into the future of AI are enlightening! The intersection of technology 
+        and ethics is particularly thought-provoking. Keep us updated on the tech front!
+        </p>
+      </div>
+    </div>
+    </div>
+  </section>
+  ))
   return (
-    <body> 
-      <Navigation/>
+    <> 
+      <Navigation/> 
        {/* Big video Image */}
+       {videoMain}
        
-       <video className='video-Image' controls poster='https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg'>
-        </video>
-        
       <main className='main'>
         <div className='main__subdivision' >   
         {/* Main Video information */}
-        <section className='main-video'>
-           <h2 className='main-video__header'> Tech Trends: The Future of Artificial Intelligence </h2>
-           <div className='main-video__info'>
-           <div className='main-video__info1'> 
-           <p className='main-video__author'> By Aiden Thompson</p>
-           <p className='main-video__date'>  8/8/2023 </p> 
-           </div> 
-           <div className='main-video__info2'>
-            <p className='main-video__views'> 980,544 </p>
-            <p className='main-video__likes'> 22,479 </p>
-           </div> 
-           </div> 
-           <div className='main-video__details'>
-           <p className='main-video__text'>
-           Explore the cutting-edge developments and predictions for Artificial Intelligence in the 
-           coming years. From revolutionary breakthroughs in machine learning to the ethical considerations 
-           influencing AI advancements, this exploration transcends the boundaries of mere speculation. 
-           Join us on a journey that navigates the intricate interplay between innovation, ethics, 
-           and the ever-evolving tech frontier.
-           </p> 
-           </div> 
-        </section>
+        {listDataMain}
+       
         {/* Ending Main Video Information */}
 
         {/* Starting Comments section */}
@@ -52,7 +85,7 @@ function App() {
         <div className="comment__subdivision">
             <img src={mohan}  className="comment__image" alt="Mohan Muruge image" />
             <form action="" className="comment__form">
-                <label for="" className="comment__form-Label"> JOIN THE CONVERSATION </label>
+                <label className="comment__form-Label"> JOIN THE CONVERSATION </label>
                 <textarea name="nameInput" className="comment__form-Input">
                  Add a new comment
                 </textarea>
@@ -96,12 +129,12 @@ function App() {
         
         {/* Ending comments section */}
         </div> 
-        <SideVideos data={data} dataChange={setData} />
+         <SideVideos data={data} dataChange={setData} /> 
       </main>
    
 
 
-    </body>
+    </>
   )
 }
 

200f10daea4aea0bc90b69e586b0fd5b0e4f8520
DanySA22 <winache22@gmail.com>
2024-02-29 19:12:55 -0800

    Do general review of the codebase 2

--- brainflix-daniel/src/App.jsx
+++ brainflix-daniel/src/App.jsx
@@ -1,24 +1,21 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import './App.scss';
-import HomePage from './pages/HomePage/HomePage';
-import UploadPage from './pages/UploadPage/UploadPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import HomePage from "./pages/HomePage/HomePage";
+import UploadPage from "./pages/UploadPage/UploadPage";
 import VideoDetailsPage from "./pages/HomePage/VideoDetailsPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
-  
   return (
-  
     <BrowserRouter>
-    <Routes>
-        <Route path = "/"  element={<HomePage/>}/>
-        <Route path = "/upload"  element = {<UploadPage/>}/>
-        <Route path = '/video/:id'  element = {<VideoDetailsPage/>}/>
-        <Route path = '*'  element = {<NotFoundPage/>}/>
-    </Routes>
-   
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/video/:id" element={<VideoDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;

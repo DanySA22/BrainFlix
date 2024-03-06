0628677abdd3592d20d4ec2ca276ce15e8f772da
DanySA22 <winache22@gmail.com>
2024-03-06 04:35:19 -0800

    Add the upload form functionality

--- brainflix-daniel/src/components/UploadForm/UploadForm.jsx
+++ brainflix-daniel/src/components/UploadForm/UploadForm.jsx
@@ -1,15 +1,65 @@
+import axios from 'axios';
+import {useState, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+import nameRandom from '../../utilities/NameRandom/NameRandom';
+import {v4 as uuidv4} from 'uuid';
 import './UploadForm.scss'     
 import videopreview from '../../assets/images/Upload-video-preview.jpg';
-import { useNavigate } from 'react-router-dom';
 
-function UploadForm({uploadDescription, uploadTitle, inputResultTitle, inputResultDescription, submitResult}) {
-  const navigate = useNavigate()  
-    
-  // function afterSubmitVideo (){
-  //   alert('Submission is succesful! You will be routed to the Main Page') 
-  //   navigate('/')
-  //   }
-  function afterCancelVideo (){
+
+function UploadForm() {
+  
+  const [uploadTitle, setUploadTitle] = useState('')
+  const [uploadDescription, setUploadDescription] = useState('')
+  const navigate = useNavigate() 
+  const inputRef = useRef(null)
+  const submitRef = useRef(null)
+
+//this function will be prop callbacks for update states uploadTitle and uploadDescription after event handler onChange on child component  gets call
+const inputResultTitle = (event) => {
+  setUploadTitle(event.target.value)
+}
+
+const inputResultDescription = (event) => {
+  setUploadDescription(event.target.value)
+}
+
+//this function will be a prop callback for POST on API after event handler onSubmit on child component gets call
+const submitResult = (event) => {
+event.preventDefault()
+try {
+const uploadVideoObject = async () => {
+const body = 
+{ id: uuidv4(),
+ title: uploadTitle,
+ channel: nameRandom(),
+image: "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image8.jpg",
+ description: uploadDescription, 
+views: "809,635",
+likes: "400059",
+duration: "12:26",
+video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
+timestamp: Date.now() ,
+comments: []
+}
+const newVideoObject = await axios.post(`http://localhost:8080/videos`, body, {
+params: {"api_key":"a32a567a-7637-4dec-9793-fd8201ce16e2"}})
+setUploadTitle('')
+setUploadDescription('')
+}
+uploadVideoObject()
+setTimeout(() => {
+  alert('Submission is succesful! You will be routed to the Main Page') 
+  navigate('/')
+}, 1000) //I put the alert and navigate in a setTimeout to make more smooth the redirection process after user click submit button
+
+} catch (error) {
+    console.error('Error submitting the form:', error)
+}
+}    
+  
+ 
+function afterCancelVideo (){
       alert('Submission has been cancelled. You will be routed to the Main Page') 
       navigate('/')
       }
@@ -23,12 +73,12 @@ function UploadForm({uploadDescription, uploadTitle, inputResultTitle, inputResu
            <img src={videopreview}  className="upload-video__thumbnail-image" alt="Mohan Muruge" />
            </div>
           
-          <form action="" className="upload-video__form" onSubmit={event => submitResult(event)} >
+          <form action="" ref={submitRef} className="upload-video__form" onSubmit={event => submitResult(event)} >
               <label className="upload-video__form-Label"> TITLE YOUR VIDEO </label>
-              <textarea  value={uploadTitle} onChange={event => inputResultTitle(event)} placeholder='Add a title to your video' name="nameInput" className="upload-video__form-Input upload-video__form-Input--height">
+              <textarea  value={uploadTitle}  onChange={event => inputResultTitle(event)} placeholder='Add a title to your video' name="title" className="upload-video__form-Input upload-video__form-Input--height">
               </textarea>
               <label className="upload-video__form-Label"> ADD A VIDEO DESCRIPTION </label>
-              <textarea  value={uploadDescription} onChange={event => inputResultDescription(event)} placeholder='Add a description to your video' name="nameInput" className="upload-video__form-Input">
+              <textarea  value={uploadDescription} onChange={event => inputResultDescription(event)} placeholder='Add a description to your video' name="description" className="upload-video__form-Input">
               </textarea>
           </form> 
         </div> 
--- brainflix-daniel/src/pages/UploadPage/UploadPage.jsx
+++ brainflix-daniel/src/pages/UploadPage/UploadPage.jsx
@@ -1,62 +1,14 @@
-import axios from 'axios';
-import {useState} from 'react';
 import Navigation from '../../components/Navigation/Navigation';
 import UploadForm from '../../components/UploadForm/UploadForm';
-import { useNavigate } from 'react-router-dom';
-import nameRandom from '../../utilities/NameRandom/NameRandom';
-import {v4 as uuidv4} from 'uuid';
+
 
 function  UploadPage() {
-  const [uploadTitle, setUploadTitle] = useState('')
-  const [uploadDescription, setUploadDescription] = useState('')
-  const navigate = useNavigate() 
-
-//this function will be prop callbacks for update states uploadTitle and uploadDescription after event handler onChange on child component  gets call
-const inputResultTitle = (event) => {
-  setUploadTitle(event.target.value)
-}
-const inputResultDescription = (event) => {
-  setUploadDescription(event.target.value)
-}
-
-//this function will be a prop callback for POST on API after event handler onSubmit on child component gets call
-const submitResult = (event) => {
-event.preventDefault()
-try {
-const uploadVideoObject = async () => {
-const body = 
-{ id: uuidv4(),
- title: uploadTitle,
- channel: nameRandom(),
-image: "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image8.jpg",
- description: uploadDescription, 
-views: "809,635",
-likes: "400059",
-duration: "12:26",
-video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
-timestamp: Date.now() ,
-comments: []
-}
-const newVideoObject = await axios.post(`http://localhost:8080/videos`, body, {
-params: {"api_key":"a32a567a-7637-4dec-9793-fd8201ce16e2"}})
-setUploadTitle('')
-setUploadDescription('')
-alert('Submission is succesful! You will be routed to the Main Page') 
-navigate('/')
-}
-uploadVideoObject()
-} catch (error) {
-    console.error('Error submitting the form:', error)
-}
-}    
+ 
   return (
         <> 
       <Navigation/> 
-      <UploadForm  uploadDescription = {uploadDescription} uploadTitle = {uploadTitle}
-      inputResultTitle = {inputResultTitle} 
-    submitResult = {submitResult} inputResultDescription = {inputResultDescription}/>
-        
-        </>
+      <UploadForm />
+      </>
     )
 }
 

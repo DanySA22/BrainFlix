edb5cbbbe5359b430cc2bb324fc2af78e1c0a40b
DanySA22 <winache22@gmail.com>
2024-03-05 19:01:56 -0800

    Add the upload form functionality 1

--- brainflix-daniel/src/components/UploadForm/UploadForm.jsx
+++ brainflix-daniel/src/components/UploadForm/UploadForm.jsx
@@ -2,13 +2,13 @@ import './UploadForm.scss'
 import videopreview from '../../assets/images/Upload-video-preview.jpg';
 import { useNavigate } from 'react-router-dom';
 
-function UploadForm() {
+function UploadForm({uploadDescription, uploadTitle, inputResultTitle, inputResultDescription, submitResult}) {
   const navigate = useNavigate()  
     
-  function afterSubmitVideo (){
-    alert('Submission is succesful! You will be routed to the Main Page') 
-    navigate('/')
-    }
+  // function afterSubmitVideo (){
+  //   alert('Submission is succesful! You will be routed to the Main Page') 
+  //   navigate('/')
+  //   }
   function afterCancelVideo (){
       alert('Submission has been cancelled. You will be routed to the Main Page') 
       navigate('/')
@@ -23,12 +23,12 @@ function UploadForm() {
            <img src={videopreview}  className="upload-video__thumbnail-image" alt="Mohan Muruge" />
            </div>
           
-          <form action="" className="upload-video__form">
+          <form action="" className="upload-video__form" onSubmit={event => submitResult(event)} >
               <label className="upload-video__form-Label"> TITLE YOUR VIDEO </label>
-              <textarea  placeholder='Add a title to your video' name="nameInput" className="upload-video__form-Input upload-video__form-Input--height">
+              <textarea  value={uploadTitle} onChange={event => inputResultTitle(event)} placeholder='Add a title to your video' name="nameInput" className="upload-video__form-Input upload-video__form-Input--height">
               </textarea>
               <label className="upload-video__form-Label"> ADD A VIDEO DESCRIPTION </label>
-              <textarea  placeholder='Add a description to your video' name="nameInput" className="upload-video__form-Input">
+              <textarea  value={uploadDescription} onChange={event => inputResultDescription(event)} placeholder='Add a description to your video' name="nameInput" className="upload-video__form-Input">
               </textarea>
           </form> 
         </div> 
@@ -36,7 +36,7 @@ function UploadForm() {
             
           <button className="upload-video__cancel-Button" onClick={afterCancelVideo}> <p className="comment__cancel-Button--text"> CANCEL </p></button>
 
-          <button className="upload-video__submit-Button" onClick={afterSubmitVideo}> <p className="comment__submit-Button--text"> PUBLISH </p></button>
+          <button type='submit' className="upload-video__submit-Button" onClick={event => submitResult(event)}> <p className="comment__submit-Button--text"> PUBLISH </p></button>
           
         </div>
       

58e8c231d3dfd4f143d52d1b43c1e0ecee8ac45d
DanySA22 <winache22@gmail.com>
2024-03-07 08:04:44 -0800

    Adding the custom video on Main Page

--- brainflix-daniel/src/components/VideoImage/VideoImage.jsx
+++ brainflix-daniel/src/components/VideoImage/VideoImage.jsx
@@ -1,11 +1,21 @@
 import './VideoImage.scss'
-  
+import defaultVideo from './BrainStationSampleVideo.mp4'
+import {useState, useRef} from 'react';
+
+
 function VideoImage({selectedVideo}){
+    console.log(selectedVideo.video)
+    const videoRef = useRef(null)
+    const videoKey = useRef(Date.now())
+    const [poster, setPoster] = useState(false)
+     
+   
+   
     return (
         <>
         <div className='video-container'>
-        <video className='video-container__Image' controls poster= {selectedVideo.image}>
-            <source src= {selectedVideo.video}  type='video/mp4'/> 
+        <video className='video-container__Video' controls poster= {selectedVideo.image}>
+        <source  src={defaultVideo} type='video/mp4'/>
         </video>
         </div>       
         </>
